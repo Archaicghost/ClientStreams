df4bb94d9b45978c738b354ff4bc965510434393
Zoe Gelech <archaic.ghost@gmail.com>
2018-09-02 19:54:49 +0000

    I don't think this code is working

--- https_example.js
+++ https_example.js
@@ -7,6 +7,11 @@ var options = {
   path: '/'
 };
 
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
 // called by https when the request is made.
 var callback = function() {
   console.log('In response handler callback!');
@@ -17,3 +22,9 @@ console.log("I'm about to make the request!");
 https.request(options, callback).end();
 
 console.log("I've made the request!");
+
+// called by https when the request is made.
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('Response: ', response);
+}
