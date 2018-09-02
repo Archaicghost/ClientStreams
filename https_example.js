a6d6cb6c77100d1794c7c2f139984ee80c25b9ac
Zoe Gelech <archaic.ghost@gmail.com>
2018-09-02 21:42:26 +0000

    Got assignment working!

--- https_example.js
+++ https_example.js
@@ -9,23 +9,6 @@ var options = {
 
 
 // called by https when the request is made.
-var callback = function() {
-  console.log('In response handler callback!');
-}
-
-console.log("I'm about to make the request!");
-
-https.request(options, callback).end();
-
-console.log("I've made the request!");
-
-// called by https when the request is made.
-var callback = function(response) {
-  console.log('In response handler callback!');
-  console.log('Response: ', response);
-}
-
-// called by https when the request is made.
 var callback = function(response) {
   console.log('In response handler callback!');
 
@@ -35,3 +18,12 @@ var callback = function(response) {
   });
 }
 
+
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
+
+// called by https when the request is made.
